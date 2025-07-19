9e1352b50600e3d9a133723320752fa9809ac3e5
calebhazelton <caleb.hazelton@gmail.com>
2025-07-18 22:12:53 -0400

    added pause and reset feature to timer

--- app.js
+++ app.js
@@ -1,36 +1,72 @@
-//Get a reference to the start and stop button
-const myButton = document.getElementById('my-button');
-
-let timerStatus = false;
-
-myButton.addEventListener('click', startTimer)
-
 function appendZero(num){
     if(num < 10){
         num = num.toString();
         num = "0" + num;
-        return num
+        return num;
     }else{
-        return num.toString()
+        return num.toString();
     }
 }
 
 function startTimer(){
-    if (timerStatus == false){
-        setTime();
+    if (countdownActive === false){
+        document.getElementById('start-button').innerHTML = 'Pause';
+        let totalTime = 0;
+        if (savedTime === 0){
+            let seconds = parseInt(document.getElementById('seconds').value, 10) || 0;
+            let minutes = parseInt(document.getElementById('minutes').value, 10) || 0;
+            totalTime = (minutes * 60) + seconds;
+
+        } else {
+
+            totalTime = savedTime
+        }
+        countdownActive = true;
+        countDown(totalTime);
     } else {
-        setTime();
+        countdownActive = false;
+        document.getElementById('start-button').innerHTML = 'Resume';
     }
 }
 
-function setTime(){
-    console.log('the function is working')
 
-    let minutes = parseInt(document.getElementById('minutes').value, 10) || 0;
+function countDown(totalTime) {
+    if (countdownActive === false) {
+        clearTimeout(timeoutID);
+        savedTime = totalTime;
+        return;
+    }
 
-    let seconds = parseInt(document.getElementById('seconds').value, 10) || 0;
-
-    console.log('timer is set for ' + minutes + ':' + seconds);
-
+    let minutes = parseInt(totalTime / 60);
+    let seconds = totalTime % 60;
     document.getElementById('countdown').innerHTML = appendZero(minutes) + ':' + appendZero(seconds);
+
+    if (totalTime === 0) {
+        return;
+    }
+
+    timeoutID = setTimeout(function () {
+        countDown(totalTime - 1);
+    }, 1000);
 }
+
+function resetTimer(){
+    clearTimeout(timeoutID);
+    totalTime = 0;
+    savedTime = 0;
+    countdownActive = false;
+    document.getElementById('start-button').innerHTML = 'Start';
+    document.getElementById('countdown').innerHTML = appendZero(0) + ':' + appendZero(0);
+
+}
+
+//Get a reference to the start and stop button
+const startButton = document.getElementById('start-button');
+const resetButton = document.getElementById('reset-button');
+
+let countdownActive = false;
+let savedTime = 0;
+let timeoutID;
+
+startButton.addEventListener('click', startTimer);
+resetButton.addEventListener('click', resetTimer)
